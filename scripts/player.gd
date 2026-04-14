005da9db5c26061b9b806dada882308cb30887cb
spanman98 <spano98dylan@gmail.com>
2026-04-14 18:38:27 -0400

    Tutorial 2 Completed

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,15 +4,36 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 var last_direction: Vector2 = Vector2.RIGHT
+var is_attacking: bool = false
+var hitbox_offset: Vector2
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var swing_sword: AudioStreamPlayer2D = $SwingSword
+@onready var hitbox: Area2D = $Hitbox
 
-#
+
+func _ready() -> void:
+	
+	# Initialize hitbox offset
+	hitbox_offset = hitbox.position
+
 
 func _physics_process(_delta: float) -> void:
+	
+	# Disable hitbox until an attack is triggered
+	hitbox.monitoring = false
+	
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		attack()
+		
+	# Skip movement if attacking
+	if is_attacking:
+		velocity = Vector2.ZERO
+		return
+		
 	process_movement()
-	move_and_slide()
 	process_animation()
+	move_and_slide()
 
 # -------------------- #
 # MOVEMENT & ANIMATION #
@@ -24,11 +45,15 @@ func process_movement() -> void:
 	if direction != Vector2.ZERO:
 		velocity = direction * SPEED
 		last_direction = direction
+		update_hitbox_offset()
 	else:
 		velocity = Vector2.ZERO
 	
 	
 func process_animation() -> void:
+	if is_attacking:
+		return
+	
 	if velocity != Vector2.ZERO:
 		play_animation("run", last_direction)
 	else:
@@ -43,3 +68,37 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 		animated_sprite_2d.play(prefix + "_up")
 	elif dir.y > 0:
 		animated_sprite_2d.play(prefix + "_down")
+	
+# Attacking #	
+		
+func attack() -> void:
+	is_attacking = true
+	hitbox.monitoring = true
+	swing_sword.play()
+	play_animation("attack", last_direction)
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if is_attacking:
+		is_attacking = false
+		
+# HITBOX #
+
+func update_hitbox_offset() -> void:
+	var x := hitbox_offset.x
+	var y := hitbox_offset.y
+	
+	match last_direction:
+		Vector2.LEFT:
+			hitbox.position = Vector2(-x, y)
+		Vector2.RIGHT:
+			hitbox.position = Vector2(x, y)
+		Vector2.UP:
+			hitbox.position = Vector2(y, -x)
+		Vector2.DOWN:
+			hitbox.position = Vector2(y, x)
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if is_attacking and body.name.begins_with("Slime"):
+		print('hit')
